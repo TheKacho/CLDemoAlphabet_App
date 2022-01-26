3a27d1b8534cf54244b7d5d04732855d99746658
TheKacho <abyssknightzero@gmail.com>
2022-01-26 12:45:32 -0500

    Add project files.

---
+++ CLDemoAlphabet_App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+Console.WriteLine("Press any key to see the regular alphabet.\n");
+
+Console.ReadKey();
+
+string alphabet = "";
+for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+{
+    alphabet += alpha;
+}
+
+Console.WriteLine($"{alphabet} \n");
+
+Console.WriteLine("Press any key to see the alphabet in reverse.\n \n");
+
+Console.ReadKey();
+
+
+string reverse = "";
+for (char alphaR = 'Z'; alphaR >= 'A'; alphaR--)
+{
+    reverse += alphaR;
+}
+
+Console.WriteLine($"{reverse} \n");
+Console.WriteLine("Press any key to see a skipped alphabet.\n \n");
+
+Console.ReadKey();
+
+string skipped = "";
+
+for (char alphaS = 'A'; alphaS <= 'Z'; alphaS++, alphaS++)
+{
+    skipped += alphaS;
+}
+
+Console.WriteLine($"{skipped} \n");
